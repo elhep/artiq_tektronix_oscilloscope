d317aa0577e10f23bb03d0a99dd2e869f9d14cdd
Mikołaj Sowiński <mikolaj.sowinski@pw.edu.pl>
2024-09-17 21:28:44 +0000

    Implemented operation queueing

--- artiq_tektronix_osc/driver.py
+++ artiq_tektronix_osc/driver.py
@@ -13,12 +13,14 @@ class Tektronix4SeriesScope:
     def __init__(self, ip_address, simulation: bool=False):
         self.ip_address = ip_address
         self._simulation = simulation
+        self.op_queue = []
     
     def __enter__(self):
         if self._simulation:
             logger.info("Running in simulation mode")
             return self
         rm = pyvisa.ResourceManager()
+        print(self.ip_address)
         self.scope = rm.open_resource(f"TCPIP::{self.ip_address}::INSTR")
         logger.info(f"Connected to scope at {self.ip_address}")
         logger.info(f"Scope identification: {self.identify()}")
@@ -29,6 +31,9 @@ class Tektronix4SeriesScope:
             logger.info("Running in simulation mode")
             return self
         self.scope.close()
+    
+    def debug(self, msg, queue):
+        logger.debug(f"{'[QUEUE] ' if queue else ''}{msg}")
 
     def identify(self):
         return self.scope.query('*IDN?')
@@ -52,119 +57,195 @@ class Tektronix4SeriesScope:
         else:
             logger.error(f"Ping failed, invalid identification: {idn}")
 
-    def reset(self):
-        logger.debug("Resetting scope")
-        self.scope.write('*RST')
-        self.wait_for_idle(20)
+    def reset(self, queue=False):
+        self.debug("Resetting scope", queue)
+        if queue:
+            self.op_queue.append('*RST')
+            self.op_queue.append('*ESE 1')
+            self.op_queue.append('*WAI')
+        else:
+            self.scope.write('*RST')
+            self.wait_for_idle(20)
 
     def get_screen_png(self):
-        logger.debug("Saving screen to PNG")
-        self.scope.write("SAVe:IMAGe:FILEFormat PNG")
-        self.scope.write("SAVe:IMAGe:INKSaver OFF")
-        self.scope.write("HARDCopy STARt")
+        self.debug("Saving screen to PNG", False)
+        self.scope.write("SAVe:IMAGe:FILEFormat PNG; :SAVe:IMAGe:INKSaver OFF; :HARDCopy STARt")
         return self.scope.read_raw()
     
-    def set_current_datetime(self):
+    def set_current_datetime(self, queue=False):
         current_datetime = datetime.datetime.now()
         current_date = current_datetime.strftime('%Y-%m-%d')
         current_time = current_datetime.strftime('%H:%M:%S')
-        logger.debug(f"Setting scope date and time to {current_date} {current_time}")
-        self.scope.write(f"DATE \"{current_date}\"")
-        self.scope.write(f"TIME \"{current_time}\"")
+        self.debug(f"Setting scope date and time to {current_date} {current_time}", queue)
+        ops = [
+            f"DATE \"{current_date}\"",
+            f"TIME \"{current_time}\""
+        ]
+        if queue:
+            self.op_queue.extend(ops)
+        else:
+            self.scope.write(";".join())
 
     # Acquisition operations
 
-    def start_acquisition(self):
-        logger.debug("Preparing acquisition")
-        self.scope.write("ACQuire:STOPAfter SEQuence")
-        self.scope.write("ACQuire:STATE ON")
+    def start_acquisition(self, queue=False):
+        self.debug("Preparing acquisition", queue)
+        ops = [
+            "ACQuire:STOPAfter SEQuence",
+            "ACQuire:STATE ON"
+        ]
+        if queue:
+            self.op_queue.extend(ops)
+        else:
+            self.scope.write(";".join(ops))
 
-    def stop_acquisition(self):
-        logger.debug("Stopping acquisition")
-        self.scope.write("ACQuire:STATE OFF")
+    def stop_acquisition(self, queue=False):
+        self.debug("Stopping acquisition", queue)
+        if queue:
+            self.op_queue.append("ACQuire:STATE OFF")
+        else:
+            self.scope.write("ACQuire:STATE OFF")
 
     # Channel operations
 
-    def set_channel_vertical_scale(self, channel, scale):
-        logger.debug(f"Setting vertical scale of channel {channel} to {scale}")
-        self.scope.write(f"CH{channel}:SCAle {scale:.3g}")
+    def set_channel_vertical_scale(self, channel, scale, queue=False):
+        self.debug(f"Setting vertical scale of channel {channel} to {scale}", queue)
+        command = f"CH{channel}:SCAle {scale:.3g}"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
     
-    def set_channel_vertical_position(self, channel, position):
-        logger.debug(f"Setting vertical position of channel {channel} to {position}")
-        self.scope.write(f"CH{channel}:POSition {position:.3g}")
+    def set_channel_vertical_position(self, channel, position, queue=False):
+        self.debug(f"Setting vertical position of channel {channel} to {position}", queue)
+        command = f"CH{channel}:POSition {position:.3g}"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
 
-    def set_channel_termination(self, channel, fifty_ohms=False):
+    def set_channel_termination(self, channel, fifty_ohms=False, queue=False):
         impedance = "FIFty" if fifty_ohms else "MEG"
-        logger.debug(f"Setting termination of channel {channel} to {impedance}")
-        self.scope.write(f"CH{channel}:IMPedance {impedance}")
+        self.debug(f"Setting termination of channel {channel} to {impedance}", queue)
+        command = f"CH{channel}:IMPedance {impedance}"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
 
-    def set_channel_label(self, channel, label):
-        logger.debug(f"Setting label of channel {channel} to {label}")
-        self.scope.write(f"CH{channel}:LABel \"{label}\"")
+    def set_channel_label(self, channel, label, queue=False):
+        self.debug(f"Setting label of channel {channel} to {label}", queue)
+        command = f"CH{channel}:LABel \"{label}\""
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
 
-    def set_channel_coupling(self, channel, ac=False):
+    def set_channel_coupling(self, channel, ac=False, queue=False):
         coupling = "AC" if ac else "DC"
-        logger.debug(f"Setting coupling of channel {channel} to {coupling}")
-        self.scope.write(f"CH{channel}:COUPling {coupling}")
+        self.debug(f"Setting coupling of channel {channel} to {coupling}", queue)
+        command = f"CH{channel}:COUPling {coupling}"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
 
-    def enable_channel_display(self, channel):
-        logger.debug(f"Enabling display of channel {channel}")
-        self.scope.write(f"SELect:CH{channel} ON")
+    def enable_channel_display(self, channel, queue=False):
+        self.debug(f"Enabling display of channel {channel}", queue)
+        command = f"SELect:CH{channel} ON"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
     
-    def disable_channel_display(self, channel):
-        logger.debug(f"Disabling display of channel {channel}")
-        self.scope.write(f"SELect:CH{channel} OFF")
+    def disable_channel_display(self, channel, queue=False):
+        self.debug(f"Disabling display of channel {channel}", queue)
+        command = f"SELect:CH{channel} OFF"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
 
     def set_channel(self, channel, enabled=True, vertical_scale=1.0, 
                     vertical_position=0.0, termination_fifty_ohms=False, 
-                    label="", ac_coupling=False):
-        self.set_channel_vertical_scale(channel, vertical_scale)
-        self.set_channel_vertical_position(channel, vertical_position)
-        self.set_channel_termination(channel, termination_fifty_ohms)
-        self.set_channel_label(channel, label)
-        self.set_channel_coupling(channel, ac_coupling)
+                    label="", ac_coupling=False, queue=False):
+        self.set_channel_vertical_scale(channel, vertical_scale, queue)
+        self.set_channel_vertical_position(channel, vertical_position, queue)
+        self.set_channel_termination(channel, termination_fifty_ohms, queue)
+        self.set_channel_label(channel, label, queue)
+        self.set_channel_coupling(channel, ac_coupling, queue)
         if enabled:
-            self.enable_channel_display(channel)
+            self.enable_channel_display(channel, queue)
         else:
-            self.disable_channel_display(channel)
+            self.disable_channel_display(channel, queue)
 
     # Timing and triggering operations
 
-    def set_horizontal_scale(self, scale):
-        logger.debug(f"Setting horizontal scale to {scale}")
-        self.scope.write(f"HORizontal:SCAle {scale:.3g}")
+    def set_horizontal_scale(self, scale, queue=False):
+        self.debug(f"Setting horizontal scale to {scale}", queue)
+        command = f"HORizontal:SCAle {scale:.3g}"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
 
-    def set_horizontal_position(self, position):
-        logger.debug(f"Setting horizontal position to {position}")
-        self.scope.write(f"HORizontal:DELay:TIMe {position:.3g}")
+    def set_horizontal_position(self, position, queue=False):
+        self.debug(f"Setting horizontal position to {position}", queue)
+        command = f"HORizontal:DELay:TIMe {position:.3g}"
+        if queue:
+            self.op_queue.append(command)
+        else:
+            self.scope.write(command)
 
-    def set_trigger(self, channel, level=0.0, slope="RISE", mode="NORMAL"):
+    def set_trigger(self, channel, level=0.0, slope="RISE", mode="NORMAL", queue=False):
         assert mode in ['NORMAL', 'AUTO'], "Invalid trigger mode"
         assert slope in ['RISE', 'FALL'], "Invalid trigger slope"
-        logger.debug(f"Setting trigger to channel {channel}, level {level}, slope {slope}, mode {mode}")
-        self.stop_acquisition()
-        self.scope.write(f"TRIGger:A:MODE {mode}")
-        self.scope.write(f"TRIGger:A:EDGE:SLOPe {slope}")
-        self.scope.write(f"TRIGger:A:EDGE:SOURce CH{channel}")
-        self.scope.write(f"TRIGger:A:LEVel {level:.3g}")
+        self.debug(f"Setting trigger to channel {channel}, level {level}, slope {slope}, mode {mode}", queue)
+        commands = [
+            f"TRIGger:A:MODE {mode}",
+            f"TRIGger:A:EDGE:SLOPe {slope}",
+            f"TRIGger:A:EDGE:SOURce CH{channel}",
+            f"TRIGger:A:LEVel {level:.3g}"
+        ]
+        if queue:
+            self.op_queue.extend(commands)
+        else:
+            self.stop_acquisition()
+            self.scope.write(";".join(commands))
 
     # One to rule them all
 
-    def setup(self, channel_configs, horizontal_scale, horizontal_position, trigger_config):
-        self.reset()
-        self.set_current_datetime()
+    def setup(self, channel_configs, horizontal_scale, horizontal_position, trigger_config, queue=False, sleep_time=3):
+        self.reset(queue)
+        self.set_current_datetime(queue)
 
         for ch_cfg in channel_configs:
-            self.set_channel(**ch_cfg)
+            self.set_channel(**ch_cfg, queue=queue)
         
         # Waveform time will be 10*horizontal scale
-        self.set_horizontal_scale(horizontal_scale)
-        self.set_horizontal_position(horizontal_position)
+        self.set_horizontal_scale(horizontal_scale, queue)
+        self.set_horizontal_position(horizontal_position, queue)
 
         # Slope: RISE/FALL
         # Mode: NORMAL/AUTO
-        self.set_trigger(**trigger_config)
-        self.start_acquisition()
+        self.set_trigger(**trigger_config, queue=queue)
+        self.start_acquisition(queue)
 
-        # Wait for the scope to be ready
-        sleep(3)
+        if not queue:
+            # Wait for the scope to be ready
+            sleep(sleep_time)
+
+    def clear_queue(self):
+        self.op_queue = []
+
+    def run_queue(self, sleep_time=3):
+        self.debug("Running queued operations", False)
+        if self._simulation:
+            logger.info("Running in simulation mode")
+            return
+        self.op_queue = [f":{cmd}" if not cmd.startswith('*') else cmd for cmd in self.op_queue]
+        command = ";".join(self.op_queue)
+        self.debug(f"Running command: {command}", False)
+        self.scope.write(";".join(self.op_queue))
+        self.clear_queue()
+        sleep(sleep_time)
